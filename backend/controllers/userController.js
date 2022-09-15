16edeb29196de70c616e189e1fed7208e9a33168
kaleighsaphire <kaleigh.scoffield@gmail.com>
2022-09-15 12:10:44 -0700

    hash and protect route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -50,15 +51,37 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+    // Check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'Display User Data'})
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn : '30d'
+    })
+}
 
 module.exports = {
     registerUser,
