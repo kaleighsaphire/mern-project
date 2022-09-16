24f465af73cfea7345b2ecae013faeb61db4748f
kaleighsaphire <kaleigh.scoffield@gmail.com>
2022-09-16 14:33:11 -0700

    hooked up login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -7,8 +12,23 @@ function Login() {
         password: ''
     })
 
-    // Destructure form data
     const {email, password} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError){
+            toast.error(message)
+        }
+        if (isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     // Function for button
     const onChange = (e) => {
@@ -21,8 +41,18 @@ function Login() {
     // Function for form 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData))
     }
 
+    if (isLoading){
+        return <Spinner />
+    }
 
   return <>
         <section className="heading">
