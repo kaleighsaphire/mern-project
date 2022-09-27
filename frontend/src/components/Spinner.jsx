70fe0e7057607e30eeecfaf6fc1d70d55c67080a
kaleighsaphire <kaleigh.scoffield@gmail.com>
2022-09-27 10:01:34 -0700

    tea cup loading animation

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,7 +1,16 @@
 function Spinner() {
   return (
-    <div className="loadingSpinnerContainer">
-        <div className="loadingSpinner"></div>
+    <div class="tea-container">
+      <div class="steam" id="steam1"></div>
+      <div class="steam" id="steam2"></div>
+      <div class="steam" id="steam3"></div>
+      <div class="steam" id="steam4"></div>
+      <div id="cup">
+        <div id="cup-body"></div>
+        <div id="cup-handle"></div>
+      </div>
+      <div id="saucer"></div>
+      <div id="shadow"></div>
     </div>
   )
 }
